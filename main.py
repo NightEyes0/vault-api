b752ce4daa1e91858502165d1dd821441c366807
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-22 23:03:47 +0100

    Add GET endpoint to fetch all secrets from SQLite

--- main.py
+++ main.py
@@ -21,16 +21,26 @@ class SecretItem(BaseModel):
 def read_root():
     return {"message": "Secure Vault API is running!"}
 
-# NEW: We now physically save the data to SQLite
 @app.post("/secrets/")
 def create_secret(item: SecretItem):
     conn = sqlite3.connect("vault.db")
     cursor = conn.cursor()
     cursor.execute("INSERT INTO secrets (title, content) VALUES (?, ?)", (item.title, item.content))
     conn.commit()
-    inserted_id = cursor.lastrowid # Get the ID of the new row
+    inserted_id = cursor.lastrowid
     conn.close()
     return {"status": "success", "id": inserted_id, "title": item.title}
+
+#  Retrieve data from the database
+@app.get("/secrets/")
+def get_secrets():
+    conn = sqlite3.connect("vault.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, content FROM secrets")
+    rows = cursor.fetchall()
+    conn.close()
+    # Convert raw SQL data into a clean list of dictionaries
+    return [{"id": row[0], "title": row[1], "content": row[2]} for row in rows]
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
