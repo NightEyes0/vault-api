fecad29d1ff0d6c6500de3629b0b33aca0e6fda6
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-22 22:23:45 +0100

    Implement SQLite database insertion for new secrets

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
-import sqlite3  #built-in SQL database engine
+import sqlite3
 
 app = FastAPI()
 
-# Create the database and 'secrets' table if they don't exist
 def init_db():
     conn = sqlite3.connect("vault.db")
     conn.execute("CREATE TABLE IF NOT EXISTS secrets (id INTEGER PRIMARY KEY, title TEXT, content TEXT)")
     conn.commit()
     conn.close()
 
-init_db() # Run every time the server starts
+init_db() 
 
 class SecretItem(BaseModel):
     title: str
@@ -22,9 +21,16 @@ class SecretItem(BaseModel):
 def read_root():
     return {"message": "Secure Vault API is running!"}
 
+# NEW: We now physically save the data to SQLite
 @app.post("/secrets/")
 def create_secret(item: SecretItem):
-    return {"status": "success", "received_title": item.title}
+    conn = sqlite3.connect("vault.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO secrets (title, content) VALUES (?, ?)", (item.title, item.content))
+    conn.commit()
+    inserted_id = cursor.lastrowid # Get the ID of the new row
+    conn.close()
+    return {"status": "success", "id": inserted_id, "title": item.title}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
