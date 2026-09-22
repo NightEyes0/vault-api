d0130f921c3fab2afc31e5d65e42e303295a2dba
Ahyan <307210285+NightEyes0@users.noreply.github.com>
2026-09-22 19:35:02 +0100

    Add POST endpoint and Pydantic data model for secrets

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel # NEW: Helps us define data structures
 import uvicorn
 
 app = FastAPI()
+
+# define "Secret" should look like
+class SecretItem(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 def read_root():
     return {"message": "Secure Vault API is running!"}
 
+# A POST route catches incoming data
+@app.post("/secrets/")
+def create_secret(item: SecretItem):
+    return {"status": "success", "received_title": item.title}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-    
